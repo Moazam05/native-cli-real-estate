04d4b9651bd307b15df6a256e0cb933d7841e8d7
Moazam05 <salmanmoazam08@gmail.com>
2024-12-25 22:49:19 +0500

    colors file

---
+++ src/constants/colors.js
@@ -0,0 +1,13 @@
+const Colors = {
+  primary: '#0061FF', // Primary 100
+  primaryLight: '#0061FF0A', // Primary 200
+  primaryMedium: '#0061FF1A', // Primary 300
+  accent: '#FBFBFD', // Accent 100
+  black: '#000000', // Black Default
+  greyLight: '#8C8E98', // Black 100
+  greyMedium: '#666876', // Black 200
+  greyDark: '#191D31', // Black 300
+  danger: '#F75555', // Danger
+};
+
+export default Colors;
