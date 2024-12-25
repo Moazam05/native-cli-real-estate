073b04416f7d7820f7a1989ba751bc17dd23f4e2
Moazam05 <salmanmoazam08@gmail.com>
2024-12-25 22:35:13 +0500

    assets

---
+++ src/constants/fonts.js
@@ -0,0 +1,8 @@
+export const Fonts = {
+  BOLD: 'Rubik-Bold',
+  EXTRA_BOLD: 'Rubik-ExtraBold',
+  LIGHT: 'Rubik-Light',
+  MEDIUM: 'Rubik-Medium',
+  REGULAR: 'Rubik-Regular',
+  SEMI_BOLD: 'Rubik-SemiBold',
+};
