096c332dc719fd28b821b9ea648e906ccf1e3dd4
Moazam05 <salmanmoazam08@gmail.com>
2024-12-27 17:53:01 +0500

    Home Page

---
+++ src/components/Searchbar/index.js
@@ -0,0 +1,89 @@
+import {
+  View,
+  TouchableOpacity,
+  Image,
+  TextInput,
+  StyleSheet,
+} from 'react-native';
+import React, {useState} from 'react';
+import {useDebouncedCallback} from 'use-debounce';
+import {useNavigation} from '@react-navigation/native';
+import icons from '../../constants/icons';
+import Colors from '../../constants/colors';
+import Fonts from '../../constants/fonts';
+
+const Searchbar = () => {
+  const navigation = useNavigation();
+  const [search, setSearch] = useState('');
+
+  const debouncedSearch = useDebouncedCallback(text => {
+    // Since we're not using Expo Router, we can handle the search differently
+    // For example, you could dispatch to Redux or handle it in the parent component
+    console.log('Debounced search:', text);
+  }, 500);
+
+  const handleSearch = text => {
+    setSearch(text);
+    debouncedSearch(text);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.searchSection}>
+        <Image source={icons.search} style={styles.searchIcon} />
+        <TextInput
+          value={search}
+          onChangeText={handleSearch}
+          placeholder="Search for anything"
+          placeholderTextColor={Colors.black300}
+          style={styles.input}
+        />
+      </View>
+
+      <TouchableOpacity>
+        <Image source={icons.filter} style={styles.filterIcon} />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '100%',
+    paddingHorizontal: 16,
+    borderRadius: 8,
+    backgroundColor: Colors.accent100,
+    borderWidth: 1,
+    borderColor: Colors.primary100,
+    marginTop: 20,
+    paddingVertical: 8,
+  },
+  searchSection: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    zIndex: 50,
+  },
+  searchIcon: {
+    width: 20,
+    height: 20,
+  },
+  input: {
+    flex: 1,
+    fontSize: 14,
+    fontFamily: Fonts.REGULAR,
+    color: Colors.black300,
+    marginLeft: 8,
+    padding: 0, // Remove default padding
+  },
+  filterIcon: {
+    width: 20,
+    height: 20,
+  },
+});
+
+export default Searchbar;
--- src/views/tabs/Home.js
+++ src/views/tabs/Home.js
@@ -1,12 +1,81 @@
-import {View, Text} from 'react-native';
+import {View, Text, SafeAreaView, Image, StyleSheet} from 'react-native';
 import React from 'react';
+import Searchbar from '../../components/Searchbar';
+import useTypedSelector from '../../hooks/useTypedSelector';
+import {selectedUser} from '../../redux/auth/authSlice';
+import icons from '../../constants/icons';
+import Fonts from '../../constants/fonts';
+import Colors from '../../constants/colors';
 
 const Home = () => {
+  const userDetails = useTypedSelector(selectedUser);
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <View style={styles.header}>
+          <View style={styles.userInfoContainer}>
+            <Image
+              source={{uri: userDetails?.user?.user_metadata?.picture}}
+              style={styles.avatar}
+            />
+            <View style={styles.userTextContainer}>
+              <Text style={styles.greetingText}>Good Morning</Text>
+              <Text style={styles.userName}>
+                {userDetails?.user?.user_metadata?.name}
+              </Text>
+            </View>
+          </View>
+          <Image source={icons.bell} style={styles.bellIcon} />
+        </View>
+
+        <Searchbar />
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  content: {
+    paddingHorizontal: 20,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: 20,
+  },
+  userInfoContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  avatar: {
+    width: 48,
+    height: 48,
+    borderRadius: 24,
+  },
+  userTextContainer: {
+    marginLeft: 8,
+    justifyContent: 'center',
+  },
+  greetingText: {
+    fontSize: 12,
+    fontFamily: Fonts.REGULAR,
+    color: Colors.black100,
+  },
+  userName: {
+    fontSize: 16,
+    fontFamily: Fonts.MEDIUM,
+    color: Colors.black300,
+  },
+  bellIcon: {
+    width: 24,
+    height: 24,
+  },
+});
 
 export default Home;
